2e4c68895ab69ed7d64d3926b5114950f8f136d0
sravanichandanala <sravanichandanala1@gmail.com>
2026-04-10 16:19:26 +0530

    Add files via upload

---
+++ python_basics_sravani.py
@@ -0,0 +1,16 @@
+name=("sravani")
+age=25
+height=5.1
+is_student=True
+print(name,age,height,is_student)
+print(f"Name:{name},Age:{age},Height:{height},student:{is_student}")
+
+
+age_months=age*12
+age_days=age*365
+remainder_age=age%7
+age_square=age**2
+print("Age in months:",age_months)
+print("Age in days:",age_days)
+print("Remainder when age devided by 7:",remainder_age)
+print("Age squard:",age_square)
